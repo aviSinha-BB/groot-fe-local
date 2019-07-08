434163295701c7042bce4908453b84526964731d
Yash Sangai <yash.sangai@bigbasket.com>
2019-07-08 10:49:37 +0530

    Few Changes

--- src/containers/redux/actions/tempAction.js
+++
@@ -1,8 +0,0 @@
-export const SET_TEMP_COMPONENT  = "set_temp_component";
-
-export const setPageData = (data) => dispatch => {
-    dispatch({
-      type: SET_TEMP_COMPONENT,
-      val: data
-    });
-};
