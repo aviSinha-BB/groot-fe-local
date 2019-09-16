6d21bc4e7d74e1b71fc49c9619c1046cd0645406
Yash Sangai <yash.sangai@bigbasket.com>
2019-09-16 10:07:41 +0530

    BB-56485 Removed Product Action from all Workflow Screens

--- src/components/ComponentStyle/SaveTempStyle.js
+++ src/components/ComponentStyle/SaveTempStyle.js
@@ -64,18 +64,6 @@ export const styles = theme => ({
         opacity: 0.9,
         marginRight: theme.spacing.unit
     },
-    formControl: {
-        margin: theme.spacing.unit,
-        [theme.breakpoints.down('sm')]: {
-            width: '217px'
-        },
-        [theme.breakpoints.up('md')]: {
-            width: '230px'
-        },
-        [theme.breakpoints.up('lg')]: {
-            width: '300px'
-        }
-    },
     uploadInputStyle: {
         display: 'none'
     },
@@ -91,11 +79,5 @@ export const styles = theme => ({
     },
     labelStyle: {
         fontFamily: 'ProximaNova-Regular',
-    },
-    inputStyle: {
-        fontFamily: 'ProximaNova-Regular'
-    },
-    inputContainer: {
-        border: 1
     }
 });
